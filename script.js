8eccab0f99161be69ff113ccf0a2be40552f3069
Reza Shoja <rezasj88@gmail.com>
2019-11-01 16:30:13 +0330

    added Javascript

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $("button").click(function() {
+    alert("Thanks for Submitting Your Email. I'll be in touche with U!");
+  });
+  $("a").click(function() {
+    alert("This Part is under Maintanance!");
+  });
+});
